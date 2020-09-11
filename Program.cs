8a115168f2eb2858738a0332f01b2befad9d5de8
student <student@C251U10>
2020-09-11 14:02:32 +0300

    removed genders.

--- Program.cs
+++ Program.cs
@@ -7,21 +7,19 @@ namespace pr01_01_up1
     public string firstName;
     public string lastName;
     public int age;
-    public Genders gender;
-    public enum Genders : int { Male, Female };
-    public Person(string _firstName, string _lastName, int _age, Genders _gender)
+    public Person(string _firstName, string _lastName, int _age)
     {
-      firstName = _firstName; lastName =
-      _lastName; age = _age; gender = _gender;
+      firstName = _firstName;
+      lastName = _lastName; age = _age;
     }
     public override string ToString()
     {
-      return firstName + " " + lastName + " (" + gender + "), age " + age;
+      return firstName + " " + lastName + ", age " + age;
     }
     static void Main(string[] args)
     {
-      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
-      Console.WriteLine(p.ToString());
+      Person p = new Person("Denis", "Allen", 32);
+      Console.WriteLine(p);
     }
   }
 }
